459ad083f1a8829f933b80d4638b896ced8eced9
Lloyd <up937789@myport.ac.uk>
2023-10-09 13:48:53 +0100

    Update milestone_3.py functions created

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -3,8 +3,8 @@ word_list = ["apple", "cherry", "melon", "grape", "banana"]
 word = random.choice(word_list)
 print(word)
 
-def check_guess():    
-    looper = True
+def ask_for_input():
+    looper = True    
     while looper == True:
         guess = input("Please enter a single letter!")
         if len(guess) == 1 and guess.isalpha() == True:
@@ -12,8 +12,13 @@ def check_guess():
             looper = False
         else:
             print("Oops! That is not a valid input.")
+    check_guess(guess)
+    
+def check_guess(guess):
+    guess = guess.lower()   
     if guess in word:
         print(f"Good guess! {guess} is in the word.")
     else:
         print(f"Sorry, {guess} is not in the word. Try again.")
 
+ask_for_input()
