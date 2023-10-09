9fb3207d34e3dc85c09e4ecaa3da6875b2c7aeee
Lloyd <up937789@myport.ac.uk>
2023-10-09 15:19:49 +0100

    Update milestone_4.py

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -6,10 +6,10 @@ class Hangman:
         word_guessed = []
         num_letters = len(set(word))
         self.num_lives = num_lives
-        word_list = ["apple", "cherry", "melon", "grape", "banana"]
+        word_list = []
         list_of_guesses = []
     
-    def ask_for_input():
+    def ask_for_input(self):
         looper = True    
         while looper == True:
             guess = input("Please enter a single letter!")
@@ -18,9 +18,9 @@ class Hangman:
                 looper = False
             else:
                 print("Oops! That is not a valid input.")
-        check_guess(guess)
+        self.check_guess(guess)
     
-    def check_guess(guess):
+    def check_guess(guess, word):
         guess = guess.lower()   
         if guess in word:
             print(f"Good guess! {guess} is in the word.")
@@ -28,4 +28,5 @@ class Hangman:
             print(f"Sorry, {guess} is not in the word. Try again.")
 
     
-
+testing = Hangman(word_list = ["apple", "cherry", "melon", "grape", "banana"], num_lives = 5)
+print(testing.ask_for_input())
