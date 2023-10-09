28cbcbbcaf0fac7cbcc3e2c249b0e34bdb0d93e8
Lloyd <up937789@myport.ac.uk>
2023-10-09 17:30:45 +0100

    Update milestone_4.py initfix

--- hangman/milestone_4.py
+++ hangman/milestone_4.py
@@ -2,31 +2,29 @@ import random
 
 class Hangman:
     def __init__(self, word_list, num_lives = 5):
-        word = random.choice(word_list)
-        word_guessed = []
-        num_letters = len(set(word))
+        self.word = random.choice(word_list)
+        self.word_guessed = []
+        self.num_letters = len(set(self.word))
         self.num_lives = num_lives
-        word_list = []
-        list_of_guesses = []
+        self.list_of_guesses = []
     
-    def ask_for_input(self):
-        looper = True    
-        while looper == True:
+    def ask_for_input(self):   
+        while True:
             guess = input("Please enter a single letter!")
             if len(guess) == 1 and guess.isalpha() == True:
                 print("Good guess!")
-                looper = False
+                break
             else:
                 print("Oops! That is not a valid input.")
         self.check_guess(guess)
     
-    def check_guess(guess, word):
+    def check_guess(self, guess):
         guess = guess.lower()   
-        if guess in word:
+        if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
         else:
             print(f"Sorry, {guess} is not in the word. Try again.")
 
     
 testing = Hangman(word_list = ["apple", "cherry", "melon", "grape", "banana"], num_lives = 5)
-print(testing.ask_for_input())
+testing.ask_for_input()
